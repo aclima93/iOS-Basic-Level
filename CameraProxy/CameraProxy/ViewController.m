22875f76759cb3c82459b1a90a502fdedd0582da
António Lima <tocalagarto93@gmail.com>
2016-04-06 00:05:03 +0100

    Added content to Timestamp app
    added iOS 8+ support for CameraProxy app

--- CameraProxy/CameraProxy/ViewController.m
+++ CameraProxy/CameraProxy/ViewController.m
@@ -26,12 +26,8 @@
 
 - (IBAction)takePhoto:(UIButton *)sender {
     if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
-        UIAlertView *myAlertView = [[UIAlertView alloc] initWithTitle:@"Error"
-                                                              message:@"Device has no camera"
-                                                             delegate:nil
-                                                    cancelButtonTitle:@"OK"
-                                                    otherButtonTitles: nil];
-        [myAlertView show];
+
+        [self showMyAlertWithTitle:@"Error" andMessage:@"Device has no camera"];
     }
     else {
 
@@ -72,5 +68,30 @@
     [picker dismissViewControllerAnimated:YES completion:NULL];
 }
 
+
+-(void)showMyAlertWithTitle:(NSString*) alertTitle andMessage:(NSString*) alertMessage{
+    
+    NSString *alertOkButtonText = @"Ok";
+    
+    if ([UIAlertController class] == nil) { //[UIAlertController class] returns NO on iOS 7 and older. You can use whatever method you want to check that the system version is iOS 8+
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:alertTitle
+                                                            message:alertMessage
+                                                           delegate:nil
+                                                  cancelButtonTitle:nil
+                                                  otherButtonTitles:alertOkButtonText, nil];
+        [alertView show];
+    }
+    else {
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:alertTitle
+                                                                                 message:alertMessage
+                                                                          preferredStyle:UIAlertControllerStyleAlert];
+        //We add buttons to the alert controller by creating UIAlertActions:
+        UIAlertAction *actionOk = [UIAlertAction actionWithTitle:alertOkButtonText
+                                                           style:UIAlertActionStyleDefault
+                                                         handler:nil]; //You can use a block here to handle a press on this button
+        [alertController addAction:actionOk];
+        [[[[UIApplication sharedApplication] keyWindow] rootViewController] presentViewController:alertController animated:YES completion:nil];
+    }
+}
 
 @end
--- Timestamp/Timestamp/AppDelegate.m
+++ Timestamp/Timestamp/AppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "AppDelegate.h"
 
+#define ROOTVIEW [[[UIApplication sharedApplication] keyWindow] rootViewController]
+
 @interface AppDelegate ()
 
 @end
@@ -23,23 +25,51 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
+    NSLog(@"applicationWillResignActive");
+    [[ROOTVIEW view] setBackgroundColor:[UIColor blueColor]];
+    [self doAnUnorthodoxDelay];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    NSLog(@"applicationDidEnterBackground");
+    [[ROOTVIEW view] setBackgroundColor:[UIColor greenColor]];
+    [self doAnUnorthodoxDelay];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    
+    NSLog(@"applicationWillEnterForeground");
+    [[ROOTVIEW view] setBackgroundColor:[UIColor yellowColor]];
+    [self doAnUnorthodoxDelay];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    NSLog(@"applicationDidBecomeActive");
+    [[ROOTVIEW view] setBackgroundColor:[UIColor redColor]];
+    [self doAnUnorthodoxDelay];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
+    NSLog(@"applicationWillTerminate");
+    [[ROOTVIEW view] setBackgroundColor:[UIColor magentaColor]];
+    [self doAnUnorthodoxDelay];
+}
+
+-(void) doAnUnorthodoxDelay{
+    
+    // Remember, this is for testing purposes only, you shouldn't abuse stupid delays such as this in a real app
+    // as it adds unnecessary complexity and possibly race conditions!
+    
+    [NSThread sleepForTimeInterval: 2.0];
 }
 
 @end
