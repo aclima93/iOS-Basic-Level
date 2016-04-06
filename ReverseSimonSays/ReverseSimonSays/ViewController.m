e92a7b05861350c7f792c1b44f434fe6a3caa6e9
António Lima <tocalagarto93@gmail.com>
2016-04-06 02:04:24 +0100

    added buttons to ReverseSimonSays app

--- ReverseSimonSays/ReverseSimonSays/ViewController.m
+++ ReverseSimonSays/ReverseSimonSays/ViewController.m
@@ -10,6 +10,12 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *myLabel;
+@property (weak, nonatomic) IBOutlet UIButton *resetButton;
+@property (weak, nonatomic) IBOutlet UIButton *redButton;
+@property (weak, nonatomic) IBOutlet UIButton *greenButton;
+@property (weak, nonatomic) IBOutlet UIButton *blueButton;
+
 @end
 
 @implementation ViewController
@@ -22,6 +28,32 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)resetButtonPressed:(id)sender {
+    [_myLabel setText:@"Press a button to change my color"];
+    [_myLabel setBackgroundColor:[UIColor whiteColor]];
+}
+
+-(IBAction)redButtonPressed:(id)sender{
+    [_myLabel setText:@"Red"];
+    [_myLabel setBackgroundColor:[UIColor redColor]];
+}
+
+- (IBAction)greenButtonPressed:(id)sender {
+    [_myLabel setText:@"Green"];
+    [_myLabel setBackgroundColor:[UIColor greenColor]];
+}
+
+- (IBAction)blueButtonPressed:(id)sender {
+    [_myLabel setText:@"Blue"];
+    [_myLabel setBackgroundColor:[UIColor blueColor]];
+}
+
+-(void)changeLabelColor:(UIColor*) color andText:(NSString*) text{
+    
+    [_myLabel setTextColor:color];
+    [_myLabel setText:text];
 }
 
 @end
